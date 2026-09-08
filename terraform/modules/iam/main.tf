070fdc09eef65448d06989bbf754cae4b35d9fef
Darwiish <inaaboowe@gmail.com>
2026-09-08 17:14:31 +0200

    Allow GitHub Actions to read ECR images

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -136,11 +136,12 @@ resource "aws_iam_role_policy" "github_actions_ecr" {
       {
         Effect = "Allow"
         Action = [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:CompleteLayerUpload",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
+           "ecr:BatchCheckLayerAvailability",
+           "ecr:BatchGetImage",
+           "ecr:CompleteLayerUpload",
+           "ecr:InitiateLayerUpload",
+           "ecr:PutImage",
+           "ecr:UploadLayerPart"
         ]
         Resource = values(var.ecr_repository_arns)
       }
