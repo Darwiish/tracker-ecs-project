dffeca95908d9a677288bf2149c42ca1763e0cfa
Darwiish <inaaboowe@gmail.com>
2026-09-10 22:53:46 +0200

    Update ECR immutability and ECS images

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecr_repository" "application" {
   for_each = local.repositories
 
   name                 = "${var.project_name}-${each.value}"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
